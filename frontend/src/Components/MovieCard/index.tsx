4540b0948b0259393fe33544f2901a267f427ef9
stcrl <insp.anv.43@gmail.com>
2022-05-15 14:08:10 -0300

    Props

--- frontend/src/Components/MovieCard/index.tsx
+++ frontend/src/Components/MovieCard/index.tsx
@@ -1,14 +1,15 @@
+import { type } from "@testing-library/user-event/dist/type";
 import MovieScore from "Components/MovieScore";
 import { Link } from "react-router-dom";
-function MovieCard() {
+import { Movie } from "types/movie";
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
+type Props = {
+    movie: Movie;
+}
+
+function MovieCard({movie}:Props) {
+     
+   
 
     return (
         <div>
